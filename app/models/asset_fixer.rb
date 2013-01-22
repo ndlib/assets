a7055641d7363b87cb6524a233aa3d59c050c9b7
Jaron Kennel <jaronkk@gmail.com>
2013-01-22 15:11:42 -0500

    Updated asset downloader

--- app/models/asset_fixer.rb
+++ app/models/asset_fixer.rb
@@ -1,5 +1,37 @@
 class AssetFixer
   URL_REGEX = /url\(['"]?([^'")]+)['"]?\)/
+  FOUNDATION_REGEX = /^\.\.\/images/
+
+  attr_accessor :version, :filename, :failed_downloads
+
+  def initialize(version, filename)
+    self.version = version
+    self.filename = filename
+    self.failed_downloads = []
+  end
+
+  def fix_assets
+    file_path = File.join(stylesheets_directory, filename)
+    replacements = []
+    css_contents = File.read(file_path)
+    matches = css_contents.scan(URL_REGEX)
+    replaced_contents = css_contents.clone
+    matches.each do |paths|
+      path = paths[0]
+      download_url = get_download_url(path)
+      file_name = File.basename(path)
+      if download_url.present?
+        if download_file(download_url)
+          replaced_contents.gsub!(path,"/assets/#{version}/#{file_name}")
+        else
+          failed_downloads << download_url
+        end
+      end
+    end
+    File.open(file_path, "w") do |file|
+      file.write(replaced_contents)
+    end
+  end
 
   def self.fix_assets(version, filename = nil)
     require 'open-uri'
@@ -11,29 +43,19 @@ class AssetFixer
     end
 
     css_files.each do |name|
-      file_path = File.join(stylesheets_directory(version), name)
-      replacements = []
-      css_contents = File.read(file_path)
-      matches = css_contents.scan(URL_REGEX)
-      replaced_contents = css_contents.clone
-      matches.each do |paths|
-        path = paths[0]
-        download_url = get_download_url(path)
-        file_name = File.basename(path)
-        if download_url.present?
-          if download_file(download_url, version)
-            replaced_contents.gsub!(path,"/assets/#{version}/#{file_name}")
-          else
-            failed_downloads << download_url
-          end
-        end
-      end
-      File.open(file_path, "w") do |file|
-        file.write(replaced_contents)
-      end
+      fixer = self.new(version, name)
+      fixer.fix_assets
     end
 
     failed_downloads
+  end
+
+  def images_directory
+    self.class.images_directory(version)
+  end
+
+  def stylesheets_directory
+    self.class.stylesheets_directory(version)
   end
 
   def self.images_directory(version)
@@ -44,22 +66,25 @@ class AssetFixer
     File.join(Rails.root,"app","assets","stylesheets",version)
   end
 
-  def self.get_download_url(path)
+  def get_download_url(path)
     if path =~ /^\/assets/
       # skip anything already in /assets
     elsif path =~ /^\//
       "https://www.library.nd.edu#{path}"
+    elsif filename == "foundation.css" && path =~ FOUNDATION_REGEX
+      new_path = path.gsub(FOUNDATION_REGEX,"/css/images")
+      get_download_url(new_path)
     else
       puts "Unknown path: #{path}"
     end
   end
 
-  def self.download_file(download_url, version)
+  def download_file(download_url)
     begin
       downloaded_file = open(download_url, "User-Agent" => "Ruby/#{RUBY_VERSION}")
       file_name = File.basename(download_url).gsub(/[?].*$/,"").gsub(/#.*$/,"")
       contents = downloaded_file.read
-      File.open(File.join(images_directory(version), file_name), "wb") do |copied_file|
+      File.open(File.join(images_directory, file_name), "wb") do |copied_file|
         copied_file.write(contents)
       end
       true
