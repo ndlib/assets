a0415ba8664f86f641ed345b3daa0e2073a26206
Jaron Kennel <jaronkk@gmail.com>
2013-01-22 10:59:49 -0500

    Including server side includes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  # Includes the relevant library SSI file from http://www.library.nd.edu/ssi/<filename>.shtml
+  def include_ssi(filepath)
+    render :partial => "/layouts/include_ssi", :locals => {:filepath => filepath}
+  end
+
+  def read_ssi_file(filepath)
+    require 'open-uri'
+    ssi_url = "http://www.library.nd.edu/#{filepath}"
+    f = open(ssi_url, "User-Agent" => "Ruby/#{RUBY_VERSION}")
+    contents = f.read
+    contents = contents.gsub(/(href|src)="\//,"\\1=\"https://www.library.nd.edu/")
+    contents
+  end
 end
