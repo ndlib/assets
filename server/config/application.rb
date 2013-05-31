ca6ba3f372ae31087fb54087af6c884d7e00ae02
Andy Wetherill <awetheri@nd.edu>
2013-05-31 11:06:13 -0400

    Adding Chemistry Library

--- server/config/application.rb
+++ server/config/application.rb
@@ -70,6 +70,7 @@ module Assets
       hesburgh_assets/main/1.0/ie.css
       hesburgh_assets/architecture_library/1.0/index.css
       hesburgh_assets/business_library/1.0/index.css
+      hesburgh_assets/chemistry_library/1.0/index.css
       hesburgh_assets/catalog/1.0/index.js
       hesburgh_assets/catalog/1.0/index.css
       hesburgh_assets/admin/1.0/index.css
