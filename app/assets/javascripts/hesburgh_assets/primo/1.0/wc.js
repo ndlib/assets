a17991c313e4b87a78f45b8205916164ff64f011
Robin Schaaf <robinmschaaf@gmail.com>
2013-08-14 15:42:38 -0400

    trying docdel again

--- app/assets/javascripts/hesburgh_assets/primo/1.0/wc.js
+++ app/assets/javascripts/hesburgh_assets/primo/1.0/wc.js
@@ -133,11 +133,13 @@ $(document).ready(function() {
 	        var dd_href = $(this).find('.EXLViewOnlineTab a').attr('href');
        		var dd_params = dd_href.substring( dd_href.indexOf('rft.') );
 		var dd_param_array = dd_params.split("&");
-console.log(dd_params);
                 var ddui = '/primo_library/libweb/tiles/local/docdel_openurl.jsp';
+
                 $.ajax({type: "get", url: ddui, dataType: "html", data: dd_params,  success: function(data){
                 	var dre = /http/;
+console.log(data)
                 	if(data.match(dre)){
+console.log('matches');
 				$(this).find('.EXLReviewsTab').after('<li id="docDelUrl" class="EXLReviewsTab EXLResultTab">' + data + '</li>');
                 	}
                 }});
