15bb96ecb8383864857df701830a3c92152c8a44
Robin Schaaf <robinmschaaf@gmail.com>
2013-08-15 13:52:22 -0400

    changes to docdel

--- app/assets/javascripts/hesburgh_assets/primo/1.0/wc.js
+++ app/assets/javascripts/hesburgh_assets/primo/1.0/wc.js
@@ -133,7 +133,10 @@ $(document).ready(function() {
 		if (lookupPNX == ''){
 		//if (($(exlResult_test).contains('.NewTNRequestTab') == false) && ($(exlResult_test).contains('.EXLRequestTab') == false)){
 	        	var dd_href = $(this).find('.EXLViewOnlineTab a').attr('href');
-			if(dd_href.indexOf('findtext') > 0){
+			//if(dd_href.indexOf('findtext') > 0){
+console.log($(this).find('.EXLViewOnlineTab a'));
+
+			if($(this).find('.EXLViewOnlineTab a').find('.findtext')){
        				var dd_params = dd_href.substring( dd_href.indexOf('?') + 1 );
 				var dd_param_array = dd_params.split("&");
                 		var ddui = '/primo_library/libweb/tiles/local/docdel_openurl.jsp';
