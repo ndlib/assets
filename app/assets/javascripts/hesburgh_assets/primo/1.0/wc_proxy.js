097dac84b39709f5c5a503392833e25aa5e62805
Robin Schaaf <robinmschaaf@gmail.com>
2013-08-21 14:45:03 -0400

    changing request ill link to requiring it not being in print instead of requestable

--- app/assets/javascripts/hesburgh_assets/primo/1.0/wc_proxy.js
+++ app/assets/javascripts/hesburgh_assets/primo/1.0/wc_proxy.js
@@ -61,21 +61,27 @@ $(document).ready(function() {
 
 		//For Doc Delivery/ILL tab
 		//only if there's no "locations" (not possible if no pnxid) tab and no online access
-		//if ((ft) && (rt.length == "0")){
-		if ((ft) && (lookupPNX == "")){
-	        	var dd_href = $(summary).find('.EXLViewOnlineTab a').attr('href');
-       			var dd_params = dd_href.substring( dd_href.indexOf('?') + 1 );
-			var dd_param_array = dd_params.split("&");
-                	var ddui = '/primo_library/libweb/tiles/local/docdel_openurl.jsp';
+		
 
-                	$.ajax({type: "get", url: ddui, dataType: "html", data: dd_params,  success: function(data){
-                		var dre = /http/;
-                		if(data.match(dre)){
-					$(summary).find('.EXLResultTabs').parents('.EXLResult').find('.EXLReviewsTab').after('<li id="docDelUrl" class="EXLReviewsTab EXLResultTab">' + data + '</li>');
-                		}
-                	}});
+		//if there's a findtext menu
+		if (ft){
+			var dd_href = $(summary).find('.EXLViewOnlineTab a').attr('href');
+                        var dd_params = dd_href.substring( dd_href.indexOf('?') + 1 );
+                        var dd_param_array = dd_params.split("&");
 		}
 
+		if (lookupPNX == ""){
+                	var ddui = '/primo_library/libweb/tiles/local/ill_request.jsp';
+		} else if(rt.length == "0"){
+                        var ddui = '/primo_library/libweb/tiles/local/docdel_openurl.jsp';
+		}
+	
+                $.ajax({type: "get", url: ddui, dataType: "html", data: dd_params,  success: function(data){
+                          var dre = /http/;
+                          if(data.match(dre)){
+                                $(summary).find('.EXLResultTabs').parents('.EXLResult').find('.EXLReviewsTab').after('<li id="docDelUrl" class="EXLReviewsTab EXLResultTab">' + data + '</li>');
+                          }
+                }}); 
            }
 
 
