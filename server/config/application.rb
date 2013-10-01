1037f9214401783be265b6a0336f4aeb38c4b765
Andy Wetherill <awetheri@nd.edu>
2013-10-01 16:02:38 -0400

    moving the default tab script to just the home page

--- server/config/application.rb
+++ server/config/application.rb
@@ -66,6 +66,7 @@ module Assets
     # config.assets.precompile += %w( search.js )
     config.assets.precompile += %w(
       hesburgh_assets/main/1.0/index.js
+      hesburgh_assets/main/1.0/tabset.js
       hesburgh_assets/main/1.0/index.css
       hesburgh_assets/main/1.0/ie.css
       hesburgh_assets/main/1.0/search-appliance.css
