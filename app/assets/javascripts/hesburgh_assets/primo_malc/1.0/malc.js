1629fa5f27e3b5f6dc99c00439c91eb3920538b6
Robin Schaaf <robinmschaaf@gmail.com>
2013-10-02 13:48:20 -0400

    fix for malc js

--- app/assets/javascripts/hesburgh_assets/primo_malc/1.0/malc.js
+++ app/assets/javascripts/hesburgh_assets/primo_malc/1.0/malc.js
@@ -650,4 +650,44 @@ function showHideDetails(){
 
 
 
+function ajHandle(){
 
+        var xmlhttp;
+
+        if (window.XMLHttpRequest){
+                // code for IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp=new XMLHttpRequest();
+        }else{
+                // code for IE6, IE5
+                xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+        }
+
+        return xmlhttp;
+
+}
+
+function performAj(xmlhttp, url, m, dat, type){
+
+        xmlhttp.onreadystatechange=function(){
+                if (xmlhttp.readyState==4 && xmlhttp.status==200){
+                        var dt = xmlhttp.responseText;
+                        if(type == "colorbox"){
+                                $.colorbox({html:dt, scrolling:false});
+                        }
+                }else if(xmlhttp.readyState==4 && xmlhttp.status != 200){
+                        if(type == "colorbox"){
+                               // setTimeout($.colorbox({html:xmlhttp.responseText}), 1000);
+                        }
+                }
+        }
+
+        if(m == 'POST'){
+                xmlhttp.open("POST",url,true);
+                xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+                xmlhttp.send(dat);
+        }else{
+                xmlhttp.open("GET",url + "&t=" + Math.random(),true);
+                xmlhttp.send();
+        }
+
+}
