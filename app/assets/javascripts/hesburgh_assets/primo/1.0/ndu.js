6ca7c1d506c16a55306d8db1da059da9627fd86c
Andy Wetherill <awetheri@nd.edu>
2013-10-07 16:56:49 -0400

    Help Link Popup

--- app/assets/javascripts/hesburgh_assets/primo/1.0/ndu.js
+++ app/assets/javascripts/hesburgh_assets/primo/1.0/ndu.js
@@ -125,11 +125,17 @@ function performAj(xmlhttp, url, m, dat, type){
                 xmlhttp.open("GET",url + "&t=" + Math.random(),true);
 		xmlhttp.send();
         }
-$('.EXLMainMenuITEMhelp').click(function() {
-    $(this).attr('onclick', 'openWindow(this.href, this.target, top=100,left=50,width=600,height=500,resizable=1,scrollbars=1); return false;');
-
-    return false;
-});
+$('.EXLMainMenuITEMhelp').click(function (event){
+ 
+                    var url = $(this).attr("href");
+                    var windowName = "Help";//$(this).attr("name");
+                    var windowSize = ['width=600,height=500,resizable=1,scrollbars=1'];
+ 
+                    window.open(url, windowName, windowSize);
+ 
+                    event.preventDefault();
+ 
+                });
 
 
 }
