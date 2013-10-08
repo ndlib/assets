f9a50b12ddc5904a25927c84740c6db983aad70a
Andy Wetherill <awetheri@nd.edu>
2013-10-08 10:33:02 -0400

    .

--- app/assets/javascripts/hesburgh_assets/primo/1.0/ndu.js
+++ app/assets/javascripts/hesburgh_assets/primo/1.0/ndu.js
@@ -79,17 +79,7 @@ $(document).ready(function(){
 
 		});
 	
-	$('.EXLMainMenuITEMhelp').click(function (event){
- 
-                    var url = $(this).attr("href");
-                    var windowName = "Help";//$(this).attr("name");
-                    var windowSize = ['width=700,height=500,resizable=1,scrollbars=1'];
- 
-                    window.open(url, windowName, windowSize);
- 
-                    event.preventDefault();
- 
-});
+	
 
 });
 
