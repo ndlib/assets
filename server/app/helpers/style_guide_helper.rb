20f29c110c16bce184ba49cb2fb843ad295f79ad
Jaron Kennel <jaronkk@gmail.com>
2013-11-25 14:04:12 -0500

    Added Include Partner Libraries checkbox to homepage nd catalog tab

--- server/app/helpers/style_guide_helper.rb
+++ server/app/helpers/style_guide_helper.rb
@@ -38,4 +38,22 @@ module StyleGuideHelper
   def lorem_ipsum_long
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   end
+
+  def search_url(search_path, url_params = {})
+    path = "/utilities/search/#{search_path}"
+    if url_params.present?
+      path += "?#{url_params.to_query}"
+    end
+    library_url(path)
+  end
+
+  def primo_search_url(path, url_params = {})
+    if path =~ /^[^\/]+$/
+      path = "ndu/#{path}"
+    end
+    if !Rails.env.production?
+      url_params[:vid] = 'NDUC'
+    end
+    search_url(path, url_params)
+  end
 end
