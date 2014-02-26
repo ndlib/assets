0f292b83f5baeeb737700781e7594ea64794a97b
Jaron Kennel <jaronkk@gmail.com>
2014-02-26 13:45:41 -0500

    Setting up primo 4 javascripts

---
+++ app/assets/javascripts/hesburgh_assets/primo4/index.js
@@ -0,0 +1,11 @@
+//
+//= require ./help.js
+//= require ./jquery.colorbox-min.js
+//= require ./jquery.simpledialog.0.1.min.js
+//= require ./jquery.tablesorter.js
+//= require ./advanced-clear-button.js
+//= require ./malc.js
+//= require ./ndu.js
+//= require ./search-partner-libraries.js
+//= require ./wc.js
+//= require ./wc_proxy.js
---
+++ app/assets/javascripts/hesburgh_assets/primo4/malc/index.js
@@ -0,0 +1,7 @@
+//
+//= require ../help.js
+//= require ../jquery.colorbox-min.js
+//= require ../jquery.simpledialog.0.1.min.js
+//= require ../jquery.tablesorter.js
+//= require ../malc.js
+//= require ../wc.js
